6b5fd21d0248639f8f4b22e4076dbd09cee40baf
Tanja <tanja7bajunovic@gmail.com>
2026-04-08 20:49:11 +0200

    profile func

--- stakeholders_service/src/main/java/com/example/stakeholders_service/service/ProfileService.java
+++ stakeholders_service/src/main/java/com/example/stakeholders_service/service/ProfileService.java
@@ -1,5 +1,6 @@
 package com.example.stakeholders_service.service;
 
+import com.example.stakeholders_service.dto.ProfileDTO;
 import com.example.stakeholders_service.model.Profile;
 import com.example.stakeholders_service.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,23 @@ public class ProfileService {
     @Autowired
     private ProfileRepository profileRepository;
 
-
     public void save(Profile profile) {
         profileRepository.save(profile);
+    }
+
+    public Profile getProfileById(Long id) {
+        return profileRepository.findByUserId(id);
+    }
+
+    public Profile updateProfile(Long userId, ProfileDTO dto) {
+        Profile profile = profileRepository.findByUserId(userId);
+
+        profile.setFirstName(dto.getFirstName());
+        profile.setLastName(dto.getLastName());
+        profile.setProfilePhoto(dto.getProfilePhoto());
+        profile.setBiography(dto.getBiography());
+        profile.setMotto(dto.getMotto());
+
+        return profileRepository.save(profile);
     }
 }
