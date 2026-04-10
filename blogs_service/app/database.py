9f92700dd39db93b5bd8d6e55690ef9eb606b305
saras27 <94789920+saras27@users.noreply.github.com>
2026-04-10 21:06:16 +0200

    dockercompose file

--- blogs_service/app/database.py
+++ blogs_service/app/database.py
@@ -1,11 +1,14 @@
-from motor.motor_asyncio import AsyncIOMotorClient
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "blog_db")
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/blog")
+STAKEHOLDERS_URL = os.getenv("STAKEHOLDERS_URL", "http://localhost:8080")
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client[DB_NAME]
+db = client.blog
 
 blogs_collection = db["blogs"]
 comments_collection = db["comments"]
