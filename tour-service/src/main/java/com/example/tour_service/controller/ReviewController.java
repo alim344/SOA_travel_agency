9940ea91d95946a731ad442b9b2e03438b7526e5
Tanja <tanja7bajunovic@gmail.com>
2026-05-15 23:14:35 +0200

    tour review

---
+++ tour-service/src/main/java/com/example/tour_service/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.example.tour_service.controller;
+
+import com.example.tour_service.DTO.ReviewDTO;
+import com.example.tour_service.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping("/{tourId}")
+    public ResponseEntity<ReviewDTO> addReview(
+            @PathVariable Long tourId,
+            @RequestBody ReviewDTO request) {
+
+        ReviewDTO response = reviewService.addReview(tourId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{tourId}")
+    public ResponseEntity<List<ReviewDTO>> getReviewsByTour(@PathVariable Long tourId) {
+        List<ReviewDTO> reviews = reviewService.getReviewsByTour(tourId);
+        return ResponseEntity.ok(reviews);
+    }
+}
---
+++ tour-service/src/main/java/com/example/tour_service/model/Review.java
@@ -0,0 +1,41 @@
+package com.example.tour_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int rating;
+
+    @Column
+    private String comment;
+
+    @Column
+    private Long touristId;
+
+    @Column
+    private LocalDateTime visitDate;
+
+    @Column
+    private LocalDateTime commentDate;
+
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "tour_id")
+    private Tour tour;
+}
