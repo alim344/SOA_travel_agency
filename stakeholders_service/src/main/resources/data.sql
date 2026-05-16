ccdebe74f908d75d4d252bc09ea2168df7dd281b
alim344 <milabudimirovic2@gmail.com>
2026-05-16 18:35:29 +0200

    Fixing stakeholders db

--- stakeholders_service/src/main/resources/data.sql
+++ stakeholders_service/src/main/resources/data.sql
@@ -1,17 +1,11 @@
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN') ON CONFLICT (name) DO NOTHING;
-INSERT INTO roles (name) VALUES ('ROLE_TOURIST') ON CONFLICT (name) DO NOTHING;
-INSERT INTO roles (name) VALUES ('ROLE_GUIDE') ON CONFLICT (name) DO NOTHING;
+-- 1. ULOGE
+INSERT INTO roles (id, name)
+SELECT 1, 'ROLE_ADMIN' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE id = 1);
 
-INSERT INTO users (username, email, password, enabled, role_id, last_password_reset_date)
-VALUES ('admin123', 'admin@gmail.com', '$2a$10$.ijdXADqb6w7MOU8iipjFujSQXDkW5HPvhmDFXxbWjhUi5QYePyiO', true, 1, now());
-INSERT INTO users (username, email, password, enabled, role_id, last_password_reset_date)
-VALUES ('mila123', 'mila@gmail.com', '$2a$10$.ijdXADqb6w7MOU8iipjFujSQXDkW5HPvhmDFXxbWjhUi5QYePyiO', true, 2, now());
-INSERT INTO users (username, email, password, enabled, role_id, last_password_reset_date)
-VALUES ('sara123', 'sara@gmail.com', '$2a$10$.ijdXADqb6w7MOU8iipjFujSQXDkW5HPvhmDFXxbWjhUi5QYePyiO', true, 3, now());
+INSERT INTO roles (id, name)
+SELECT 2, 'ROLE_TOURIST' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE id = 2);
 
-INSERT INTO profiles (first_name, last_name, profile_photo, biography, motto, user_id)
-VALUES ('Tanja', 'Bajnunovic', 'slika_tanje.jpg', null, null, 1);
-INSERT INTO profiles (first_name, last_name, profile_photo, biography, motto, user_id)
-VALUES ('Mila', 'Budimirovic', 'slika_mile.jpg', 'biografija_mile', 'motto_mile', 2);
-INSERT INTO profiles (first_name, last_name, profile_photo, biography, motto, user_id)
-VALUES ('Sara', 'Sapundzija', 'slika_sare.jpg', 'biografija_sare', 'motto_sare', 3);
+INSERT INTO roles (id, name)
+SELECT 3, 'ROLE_GUIDE' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE id = 3);
+
+
